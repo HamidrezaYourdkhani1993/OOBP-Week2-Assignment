f81be42c9b14c72528c933df50987f7da5f0d93e
Hamidreza Yourdkhani <110778286+HamidrezaYourdkhani1993@users.noreply.github.com>
2023-01-22 15:18:00 +0200

    Update week2
    I did my week 2 task

--- week2.ts
+++ week2.ts
@@ -9,7 +9,9 @@ Task 1 - Greeter function
 Create a function with the name GreeterFunction that takes a string as a parameter and returns a string that says "Hello, " + the parameter.
 */
 /* Write your Task 1 solution here */
-
+function GreeterFunction(name: string): string {
+  return "Hello, " + name;
+}
 /* 
 Task 2 - Define a city object and use an interface to define the object structure.
 The city object should have the following properties
@@ -21,9 +23,19 @@ The city object should be assigned to the cityObject variable.
 
 The city object should describe Helsinki, Finland with population of 600000.
 */
+interface City {
+  name: string;
+  population: number;
+  country: string;
+}
+let city : City = {
+  name: "Helsinki",
+  population: 600000,
+  country: "Finland",
+}
 
-let cityObject = null; // Assign your city object to cityObject variable.
-
+let cityObject = city; // Assign your city object to cityObject variable.
+console.log(cityObject);
 /*
 Task 3 - Create a class to describe an animal.
 The animal class should have the following properties:
@@ -34,13 +46,19 @@ Then create two new animal objects, which should be assigned to animal1 and anim
 animal1 should have name "Cat" and age 2
 animal2 should have name "Dog" and age 3
 */
-class Animal {
+
   /* Write your solution here */
-}
-
-let animal1 = null; // initialize the animal1
-let animal2 = null; // initialize the animal2
-
+  class Animal {
+    name: string;
+    age: number;
+    constructor(name: string, age: number) {
+      this.name = name;
+      this.age = age;
+    }
+  }
+  let animal1 = new Animal("Cat", 2);
+  let animal2 = new Animal("Dog", 3);
+  console.log(animal1+ " " + animal2);
 /* Task 4 - Improve your solution for Task 3 so that the Animal
   class has a method called "makeSound". This improved solution should be implemented 
   in a class named Animal2. 
@@ -57,9 +75,22 @@ let animal2 = null; // initialize the animal2
 */
 class Animal2 {
   /* Write your solution here */
+  name: string;
+  sound: string;
+  age: number;
+  constructor(name:string, sound:string,age:number){
+    this.name = name;
+    this.sound = sound;
+    this.age = age;
+  }
+  makeSound():string{
+    return `I am a ${this.name} and I sound like ${this.sound}`;
+  }
 }
 
-let snake = null; // Initialize the snake
-let lion = null; // Initialize the lion
+let snake = new Animal2("Snake","hiss",4); // Initialize the snake
+let lion = new Animal2("Lion","roar",8); // Initialize the lion
+console.log(snake.makeSound());
+console.log(lion.makeSound());
 
 export { GreeterFunction, cityObject, animal1, animal2, snake, lion };
